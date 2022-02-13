bf3af6ea77c7755dddb81dbebe6df0174f86635d
André Guilherme de Oliveira Alcantaa <andre.alcantara@reitoria.ifpe.edu.br>
2022-02-13 18:15:09 -0300

    a

---
+++ apps/play/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Play
+
+
+# Register your models here.
+admin.site.register(Play)
---
+++ apps/play/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+class Play(models.Model):
+    teste = models.CharField(max_length=50)
